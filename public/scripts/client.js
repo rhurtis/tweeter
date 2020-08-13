b9d62da33f0f19ec0ff1e564a6caba3ea07a2bad
Ryan H <rhurtis@hotmail.com>
2020-08-13 21:24:58 +0000

    Added dynamic tweets and fixed the hovering effect to toggle the users handle.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,91 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const sampleTweetObjects =  [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+const sampleTweetObject = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png"
+    ,
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
+
+
+ $(document).ready(function() {
+
+  const createTweetElement = function (tweet) {
+   const tweetElement = `<div class='whole-tweet'>
+   <article> 
+       <div class='user-information'>
+         <h3 class='actual-name'>
+           <picture>
+            <img src=${tweet['user']['avatars']}>
+           </picture>
+           ${tweet['user']['name']}
+         </h3>
+         <h3 class='tweeter-handle'>${tweet['user']['handle']}</h3>
+       </div>
+       <p>${tweet['content']['text']}</p>
+       
+       <div class='utility'> 
+         <div class='time'>${tweet['created_at']}</div>
+         <div class='misc-buttons'>
+           <button>Retweet</button> 
+           <button>Save</button> 
+           <button>Report</button> 
+         
+         </div>
+
+       </div>
+
+   </article>
+ </div>`
+
+ $(".tweet-holder").append(tweetElement);
+
+  }
+
+    
+  const renderTweets = function (tweets) {
+    
+    for(let tweet of tweets) {
+      createTweetElement(tweet)
+
+    }
+  }
+  renderTweets(sampleTweetObjects);
+
+
+
+ });
+  
+
