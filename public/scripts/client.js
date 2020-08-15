d6053c0873b542ffe4ad0beec173f339e5ec6606
Ryan H <rhurtis@hotmail.com>
2020-08-15 02:51:30 +0000

    Cleaned up the code for submission.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,49 +4,11 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// const sampleTweetObjects =  [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ];
 
-// const sampleTweetObject = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png"
-//     ,
-//     "handle": "@SirIsaac"
-//   },
-//   "content": {
-//     "text": "If I have seen further it is by standing on the shoulders of giants"
-//   },
-//   "created_at": 1461116232227
-// }
+$(document).ready(function() {
 
-
- $(document).ready(function() {
-
-  const createTweetElement = function (tweet) {
-   const tweetElement = `<div class='whole-tweet'>
+  const createTweetElement = function(tweet) {
+    const tweetElement = `<div class='whole-tweet'>
    <article> 
        <div class='user-information'>
          <h3 class='actual-name'>
@@ -71,58 +33,41 @@
        </div>
 
    </article>
- </div>`
+ </div>`;
 
- //$(".tweet-holder").append(tweetElement);
- $(".tweet-holder").prepend(tweetElement);
-  }
+    //$(".tweet-holder").append(tweetElement);
+    $(".tweet-holder").prepend(tweetElement);
+  };
 
-    // renderTweets loops through an array of objects, using createTweetElement to append data to the site.
-  const renderTweets = function (tweets) {
-      for(let tweet of tweets) {
-        createTweetElement(tweet);
+  // renderTweets loops through an array of objects, using createTweetElement to append data to the site.
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      createTweetElement(tweet);
     }
-  }
-  //renderTweets(sampleTweetObjects);
-  
+  };
 
-  //creating an ajax post request to work with the tweet button.
-  //   $( "#new-tweet-form" ).submit(function( event ) {
-  //   //alert( "Handler for .submit() called." );
-  //   event.preventDefault();
-  //   $.ajax('/tweets', {method:'POST', data: $( this ).serialize() })
-  // });
-
-  // checking text area before submission
-    //creating an ajax post request to work with the tweet button.
-    
     
       
-     $( "#new-tweet-form" ).submit(function( event ) {
+  $("#new-tweet-form").submit(function(event) {
 
-        event.preventDefault();
-        let charactersT = $('#tweet-text').val();
-        console.log(charactersT);
-         if (charactersT.length === 0) {
-          //alert( "you can't enter blank tweet" );
-         // $('#error-message').css('opacity','1');
-          $('#error-message1').slideDown(1000);
-         } else if (charactersT.length > 140) {
-          //alert( "you've exceed 140 characters" );
-          //$('#error-message').css('opacity','1').slideDown(5000);
-          $('#error-message2').slideDown(1000);
+    event.preventDefault();
+    let charactersT = $('#tweet-text').val();
+    console.log(charactersT);
+    if (charactersT.length === 0) {
+      $('#error-message1').slideDown(1000);
+    } else if (charactersT.length > 140) {
+      $('#error-message2').slideDown(1000);
           
-         } else {   
-          $.ajax('/tweets', {method:'POST', data: $( this ).serialize() });
-          
-          
-          
-          $('#error-message1').slideUp(1000);
-          $('#error-message2').slideUp(1000);
-          setTimeout(() => {location.reload()}, 1001);
-        };
+    } else {
+      $.ajax('/tweets', {method:'POST', data: $(this).serialize() });                 
+      $('#error-message1').slideUp(1000);
+      $('#error-message2').slideUp(1000);
+      setTimeout(() => {
+        location.reload();
+      }, 1001);
+    }
       
-    });
+  });
 
 
 
@@ -135,16 +80,16 @@
 
 
   //loadTweets; a fcn responsible for fetching tweets from the /tweets page.
-  const loadTweets = function () {
-   return $.ajax('/tweets',{method: 'GET', data: $( this ).serialize() }).then(function (tweetResponse) {
+  const loadTweets = function() {
+    return $.ajax('/tweets',{method: 'GET', data: $(this).serialize() }).then(function(tweetResponse) {
       renderTweets(tweetResponse);
-    })
+    });
     
   };
   
   loadTweets().then();
-  //renderTweets(loadTweets);
- });
+  
+});
   
 
 // XSS prevention
@@ -157,13 +102,12 @@ const escape =  function(str) {
 
 
 // getting time in a readable way
-const theTimeIs = function(){
-let date = new Date();
-let minutes = date.getMinutes();
-let hour = date.getHours();
-let day = date.getDate();
-let month = date.getMonth();
-let year = date.getFullYear();
-return `${hour}:${minutes} ${day}-${month + 1} -${year}`;
-}
-// tweet['created_at'] this works
+const theTimeIs = function() {
+  let date = new Date();
+  let minutes = date.getMinutes();
+  let hour = date.getHours();
+  let day = date.getDate();
+  let month = date.getMonth();
+  let year = date.getFullYear();
+  return `${hour}:${minutes} ${day}-${month + 1} -${year}`;
+};
